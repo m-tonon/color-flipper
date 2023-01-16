09749e2a881dac24a08be33b5a550f814965c88c
m-tonon <math.tonon@gmail.com>
2023-01-16 07:20:35 -0300

    improving simple color flipper logic

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,24 @@
-const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+// const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
 
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
 btn.addEventListener('click', function() {
   // goal => get random number between 0 -3 (n index) colors [i]
-  const randomNumber = getRandomNumber();
-  console.log(randomNumber);
+  const pickedColor = getRGBColor();
+  console.log(pickedColor);
 
-  document.body.style.backgroundColor = colors[randomNumber];
-  color.textContent = colors[randomNumber];
+  document.body.style.backgroundColor = pickedColor;
+  color.textContent = pickedColor;
 });
 
-function getRandomNumber () {
-  return Math.floor(Math.random()*colors.length);
+// function getRandomNumber () {
+//   return Math.floor(Math.random()*colors.length);
+// }
+function getRGBColor(){
+  const number = (min,max) => Math.floor((Math.random() * max) + min);
+  const r = number(0,255);
+  const g = number(0,255); 
+  const b = number(0,255);
+  return rgb = `rgb(${r},${g},${b})`;
 }
